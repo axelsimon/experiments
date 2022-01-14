47b7d84b992aafb86e2e48be7b4c90ed759ffd5b
axel simon <git@axelsimon.net>
2022-01-14 18:08:42 +0000

    First commit
    Toy TPM utility that returns a given number of random bytes
    
    Signed-off-by: axel simon <git@axelsimon.net>

---
+++ tpm/main.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/google/go-tpm/tpm2"
+)
+
+func main() {
+	f, err := os.OpenFile("/dev/tpmrm0", os.O_RDWR, 0)
+	if err != nil {
+		log.Fatalf("Opening TPM: %v\nDo you have sufficient permissions?", err)
+	}
+	defer f.Close()
+
+	var reqb uint
+	flag.UintVar(&reqb, "n", 16, "Number of random bytes to return from TPM")
+	flag.Parse()
+
+	reqb16 := uint16(reqb)
+	out, err := tpm2.GetRandom(f, reqb16)
+	if err != nil {
+		log.Fatalf("Getting random bytes from TPM: %v", err)
+	}
+
+	fmt.Printf("%x\n", out)
+}
